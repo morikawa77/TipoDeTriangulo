dd563f14abd3381a8338dbc5e1a1255189d3d4d0
REGINALDO MORIKAWA <reginaldo.morikawa@senacsp.edu.br>
2017-08-21 17:20:28 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace TipoDeTriangulo
     {
         static void Main(string[] args)
         {
+            int lado1, lado2, lado3;
+            Console.Write("Digite o lado 1: ");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o lado 2: ");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o lado 3: ");
+            lado3 = Convert.ToInt32(Console.ReadLine());
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.Write("Equilátero");
+            }
+            else if(lado1 == lado2 || lado2 == lado3 || lado3 == lado1)
+            {
+                Console.Write("Isósceles");
+            }
+            else
+            {
+                Console.Write("Escaleno");
+            }
+            Console.ReadLine();
         }
     }
 }
