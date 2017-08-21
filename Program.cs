b52ff59e8668372ec10e318ca509f87570f36cb6
Reginaldo.Morikawa <Reginaldo.Morikawa@SJR0561267W10-1>
2017-08-21 17:08:36 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDeTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
